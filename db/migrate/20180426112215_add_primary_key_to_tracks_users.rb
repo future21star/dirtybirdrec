7c19d92a37193d36bf9b926fe21b345912ca61c7
Victor <ostorodonodor@gmail.com>
2018-04-26 18:54:45 +0700

    Fix

--- db/migrate/20180426112215_add_primary_key_to_tracks_users.rb
+++ db/migrate/20180426112215_add_primary_key_to_tracks_users.rb
@@ -1,6 +1,6 @@
 class AddPrimaryKeyToTracksUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :tracks_users, :id, :bigint
-    # execute "ALTER TABLE tracks_users ADD PRIMARY KEY (id);"
+    # add_column :tracks_users, :id, :bigint
+    execute "ALTER TABLE tracks_users ADD PRIMARY KEY (id);"
   end
 end
